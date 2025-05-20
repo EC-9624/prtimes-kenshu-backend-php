10afb2629de8cbaa260b9ba83020a32d362a44a3
eakudompong.chanoknan <ceguantz96@outlook.com>
2025-05-20 12:31:38 +0900

    fix: typo

--- db/init.sql
+++ db/init.sql
@@ -3,7 +3,8 @@ CREATE TABLE "users" (
   "user_name" varchar(255) NOT NULL,
   "email" varchar(255),
   "password" varchar(255) NOT NULL,
-  "created_at" datetime NOT NULL DEFAULT (now())
+  "created_at" datetime NOT NULL DEFAULT (now()),
+  "updated_at" datetime DEFAULT NULL
 );
 
 CREATE TABLE "posts" (
