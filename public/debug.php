c791ebeed96a2e2f8efe1e1fafb3218a15fd84b5
eakudompong.chanoknan <ceguantz96@outlook.com>
2025-05-23 10:27:08 +0900

    feat: remove debug

--- public/debug.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Method: <pre>" . $_SERVER['REQUEST_METHOD'] . "</pre>\n";
-echo "URI: <pre>" . $_SERVER['REQUEST_URI'] . "</pre>\n";
-echo "Server: <pre>" . print_r($_SERVER, true) . "</pre>";
