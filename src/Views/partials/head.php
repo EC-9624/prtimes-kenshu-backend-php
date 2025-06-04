f231b5480090bf35c6ec5bc13dc6e07c01cf8119
eakudompong.chanoknan <ceguantz96@outlook.com>
2025-06-04 09:38:48 +0900

    refactor tailwindcss

--- src/Views/partials/head.php
+++ src/Views/partials/head.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/js/tailwindcss.js"></script>
+    <link rel="stylesheet" href="/css/style.css">
 
     <title><?= htmlspecialchars($title) ?></title>
 </head>
